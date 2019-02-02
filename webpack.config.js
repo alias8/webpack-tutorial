66da5ae920998a040c45bf11dbb504d6fe785771
James Kirk <jameskirk8@gmail.com>
2019-02-02 20:02:52 +1100

    Styling

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   target: 'web',
   mode: 'development',
   devtool: "inline-source-map",
+     module: {
+       rules: [
+           {
+             test: /\.css$/,
+             use: [
+               'style-loader',
+               'css-loader'
+             ]
+         }
+       ]
+     }
 };
