dc54ebce03f41065231464e3f02295c18dcc4126
James Kirk <jameskirk8@gmail.com>
2019-02-03 10:16:14 +1100

    use minicssextractplugin, lodashModuleReplacementPLugin, BundleAnalzer, trying to get tree shaking to work

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import _ from 'lodash';
-import './style.css';
+// import _ from 'lodash';
+// import './style.css';
+import {cube} from './math.js';
 
 
 function component() {
   let element = document.createElement('div');
 
+  // using babel-plugin-lodash, webpack cherry picks join to package and not the entire lodash library
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
   element.classList.add('hello');
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
   entry: './src/index.js',
@@ -7,17 +11,51 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   target: 'web',
-  mode: 'development',
+  mode: 'production',
   devtool: "inline-source-map",
-     module: {
-       rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-         }
-       ]
-     }
+  // optimization: {
+  //   usedExports: true
+  // },
+  optimization: {
+    minimize: false,
+    sideEffects: false,
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'babel-loader',
+      //       options: {
+      //         plugins: ['lodash'],
+      //         presets: [['env', {'modules': false, 'targets': {'node': 10}}]]
+      //       }
+      //     }
+      //   ],
+      // },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // options: {
+            //   // you can specify a publicPath here
+            //   // by default it use publicPath in webpackOptions.output
+            //   publicPath: '../'
+            // }
+          },
+          // 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    // new LodashModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin()
+  ],
+
 };
